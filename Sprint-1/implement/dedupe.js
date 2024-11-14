7e67c0b63ec5d9f9856ca9cd35a345ac01f368b2
shaden-pr <pratiamalden@gmail.com>
2024-11-14 23:09:45 +0000

    completed dedupe function

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,16 @@
-function dedupe() {}
+function dedupe(array) {
+    if (array.length === 0) {
+    return [];
+    }
+
+
+    const uniqueArray = [...new Set(array)];
+    // Set automatically removes duplicate values
+    // new Set(array) creates new set containing only the unique elm from array
+    // [] used to spread operator to convert the set back into an array format
+    if (array.length === uniqueArray.length) {
+        return array;
+    }
+
+    return uniqueArray;
+}
--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -21,7 +21,9 @@ test.todo("given an empty array, it returns an empty array");
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
+test.todo("given an empty array, it returns an empty array");
 
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
+test.todo("given an array with duplicates, it removes duplicates and preserves the first occurrence of each element");
