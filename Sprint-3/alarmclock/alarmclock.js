2e66c0f20ff4cf34042165110279ad262781bd21
shaden-pr <pratiamalden@gmail.com>
2024-12-06 16:49:06 +0000

    Make an Alarm Clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,31 @@
-function setAlarm() {}
+function setAlarm() {
+
+  let alarmInput = parseInt(document.getElementById("alarmSet").value, 10);
+
+  if(isNaN(alarmInput) || alarmInput <= 0) {
+    alert("Please enter a valid time");
+    return;
+  }
+
+  const timeRemaining = document.getElementById("timeRemaining");
+
+  timeRemaining.innerText = `Time Remaining: ${String(Math.floor(alarmInput / 60)).padStart(2, "0")}:${String(alarmInput % 60).padStart(2, "0")}`;
+    
+  const intervalID = setInterval(() => {
+    alarmInput--;
+
+    if(alarmInput > 0){
+      timeRemaining.innerText = `Time Remaining: ${String(
+        Math.floor(alarmInput / 60)
+      ).padStart(2, "0")}:${String(alarmInput % 60).padStart(2, "0")}`;
+    } else {
+      clearInterval(intervalID);
+      timeRemaining.innerText = "Time Remaining: 00:00";
+      playAlarm();
+      startFlashingBackground();
+    }
+    }, 1000);
+  }
 
 // DO NOT EDIT BELOW HERE
 
