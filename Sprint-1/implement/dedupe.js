e88356f735a6b651c003a150f29c3a187112641a
shaden-pr <pratiamalden@gmail.com>
2024-12-09 13:03:02 +0000

    update and fixed dedupe function/test

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,16 +1,8 @@
 function dedupe(array) {
-    if (array.length === 0) {
+  if (array.length === 0) {
     return [];
-    }
-
-
-    const uniqueArray = [...new Set(array)];
-    // Set automatically removes duplicate values
-    // new Set(array) creates new set containing only the unique elm from array
-    // [] used to spread operator to convert the set back into an array format
-    if (array.length === uniqueArray.length) {
-        return array;
-    }
-
-    return uniqueArray;
+  }
+  return [...new Set(array)]; // Use a Set to remove duplicates and return the unique array
 }
+
+module.exports = dedupe;
