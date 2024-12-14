55a1f0947c8b04d4e425e239cda483d8861ce394
shaden-pr <pratiamalden@gmail.com>
2024-12-14 19:04:48 +0000

    Add reading list with book details and styling

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,48 @@ const books = [
   },
 ];
 
+function readingList(books) {
+  // select the <ul> reading list element
+  const readingList = document.getElementById("reading-list");
+
+  // iterate through the array
+  books.forEach((book) => {
+    // create a list item for each book
+    const listItem = document.createElement("li");
+    listItem.style.padding = "1rem";
+    listItem.style.marginBottom = "1rem";
+    listItem.style.alignItems = "center";
+    listItem.style.listStyleType = "disc";
+
+    // background color based on whether the book is already read
+    listItem.style.backgroundColor = book.alreadyRead ? "green" : "red";
+
+    // create img element for the book cover
+    const bookImage = document.createElement("img");
+    bookImage.src = book.bookCoverImage;
+
+    // create container for the book details
+    const bookDetails = document.createElement("div");
+
+    // add book title
+    const bookTitle = document.createElement("h3");
+    bookTitle.innerText = book.title;
+    bookTitle.style.marginBottom = "0";
+
+    // add book author
+    const bookAuthor = document.createElement("p");
+    bookAuthor.innerText = `by ${book.author}`;
+    bookAuthor.style.marginTop = "0";
+    bookAuthor.style.marginBottom = "1rem";
+
+    bookDetails.appendChild(bookTitle);
+    bookDetails.appendChild(bookAuthor);
+
+    listItem.appendChild(bookDetails);
+    listItem.appendChild(bookImage);
+
+    readingList.appendChild(listItem);
+  });
+}
+// call the function to populate the reading list
+readingList(books);
