29007ecce835f8f21cf2118d4dbec63a196400a3
shaden-pr <pratiamalden@gmail.com>
2025-01-03 16:32:48 +0000

    fix alarm clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,31 +1,41 @@
+let isTimerRunning = false; // Global variable to track timer status
+
 function setAlarm() {
+  if (isTimerRunning) {
+    alert("A timer is already running.");
+    return;
+  }
 
   let alarmInput = parseInt(document.getElementById("alarmSet").value, 10);
 
-  if(isNaN(alarmInput) || alarmInput <= 0) {
+  if (isNaN(alarmInput) || alarmInput <= 0) {
     alert("Please enter a valid time");
     return;
   }
 
+  isTimerRunning = true; // set timer state to running
   const timeRemaining = document.getElementById("timeRemaining");
 
-  timeRemaining.innerText = `Time Remaining: ${String(Math.floor(alarmInput / 60)).padStart(2, "0")}:${String(alarmInput % 60).padStart(2, "0")}`;
-    
+  timeRemaining.innerText = `Time Remaining: ${String(
+    Math.floor(alarmInput / 60)
+  ).padStart(2, "0")}:${String(alarmInput % 60).padStart(2, "0")}`;
+
   const intervalID = setInterval(() => {
     alarmInput--;
 
-    if(alarmInput > 0){
+    if (alarmInput > 0) {
       timeRemaining.innerText = `Time Remaining: ${String(
         Math.floor(alarmInput / 60)
       ).padStart(2, "0")}:${String(alarmInput % 60).padStart(2, "0")}`;
     } else {
       clearInterval(intervalID);
+      isTimerRunning = false; // reset timer state
       timeRemaining.innerText = "Time Remaining: 00:00";
       playAlarm();
       startFlashingBackground();
     }
-    }, 1000);
-  }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
